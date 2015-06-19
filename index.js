4ba0cf0f0367d49f26576b769adc6dbe6224143b
Brad Murry <bradodarb@hotmail.com>
2015-06-18 21:11:29 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,13 @@
+var Hapi = require('hapi');
+//var models = require('./models');
+var port = process.env.PORT || 3000;
+// create the server
+var server = new Hapi.Server();
+server.connection({ port : port })
+
+// routes
+//server.route(require('./lib/routes'));
+ 
+server.start(function() {
+    console.log('Running on ' + port);
+  });
