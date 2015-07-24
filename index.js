d307344e222c563c8fc02f45adbbb988dd4c46f5
Brad Murry <bradodarb@hotmail.com>
2015-07-23 17:14:25 -0700

    updated search mappings

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+
 //var models = require('./models');
 var port = process.env.PORT || 3000;
 // create the server
@@ -9,5 +10,8 @@ server.connection({ port : port })
 server.route(require('./lib/routes'));
  
 server.start(function() {
-    console.log('Running on ' + port);
-  });
+	console.log('Running on ' + port);
+});
+
+
+ 
